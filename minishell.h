3abc1e3ceaaae579c230a7d913e8e43dc44c499c
almelo <algacyr.junior@gmail.com>
2023-02-01 23:29:36 -0300

    Create a header file

--- minishell.h
+++ minishell.h
@@ -1,29 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: almelo <almelo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/01 23:15:15 by almelo            #+#    #+#             */
-/*   Updated: 2023/02/01 23:27:03 by almelo           ###   ########.fr       */
+/*   Created: 2023/02/01 23:23:55 by almelo            #+#    #+#             */
+/*   Updated: 2023/02/01 23:28:43 by almelo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-int	main(void)
-{
-	char	*input;
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
 
-	while (42)
-	{
-		input = readline("minishell> ");
-		if (strcmp(input, "exit") == 0)
-			break ;
-		add_history(input);
-		free(input);
-	}
-	free(input);
-	return (0);
-}
+#endif
