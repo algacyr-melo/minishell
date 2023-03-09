649f36cade4c89103c639a4166458a615ddc2f64
almelo <algacyr.junior@gmail.com>
2023-03-09 13:45:25 -0300

    Improve env removal

--- unset.c
+++ unset.c
@@ -6,7 +6,7 @@
 /*   By: almelo <almelo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 18:00:34 by almelo            #+#    #+#             */
-/*   Updated: 2023/03/07 18:38:18 by almelo           ###   ########.fr       */
+/*   Updated: 2023/03/09 13:40:08 by almelo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,22 @@
 static t_env	*remove_env(t_envl *env_lst, char *key)
 {
 	t_env   *tmp;
-	t_env   *node;
+	t_env	*prev;
 
 	tmp = env_lst->head;
-	if (ft_strcmp(tmp->key, key) == 0)
+	prev = NULL;
+	while (tmp)
 	{
-		env_lst->head = tmp->next;
-		env_lst->length--;
-		return (tmp);
-	}
-	while (tmp->next)
-	{
-		if (ft_strcmp(tmp->next->key, key) == 0)
+		if (ft_strcmp(tmp->key, key) == 0)
 		{
-			node = tmp->next;
-			tmp->next = tmp->next->next;
+			if (prev)
+				prev->next = tmp->next;
+			else
+				env_lst->head = tmp->next;
 			env_lst->length--;
-			return (node);
+			return (tmp);
 		}
+		prev = tmp;
 		tmp = tmp->next;
 	}
 	return (NULL);
