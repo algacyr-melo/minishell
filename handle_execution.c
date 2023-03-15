7c1b5b99019c698e6218aa54f79e8a4c0213fcb6
almelo <algacyr.junior@gmail.com>
2023-03-14 23:29:22 -0300

    Bug from calling 'cd' inside a child process

--- handle_execution.c
+++ handle_execution.c
@@ -6,7 +6,7 @@
 /*   By: almelo <almelo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 15:54:04 by almelo            #+#    #+#             */
-/*   Updated: 2023/03/14 19:42:00 by almelo           ###   ########.fr       */
+/*   Updated: 2023/03/14 23:26:32 by almelo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ void	handle_pipe(char **argv, char **envp, t_envl *env_lst, int *prevpipe)
 	char	*pathname;
 
 	pipe(pipefd);
+	//handle_builtin(argv, envp, env_lst);
 	pid = fork();
 	if (pid == 0)
 	{
@@ -52,12 +53,33 @@ void	handle_pipe(char **argv, char **envp, t_envl *env_lst, int *prevpipe)
 	}
 }
 
+void	handle_last_cmd(char **argv, char **envp, t_envl *env_lst, int *prevpipe)
+{
+	pid_t	pid;
+	char	*pathname;
+
+	pid = fork();
+	//handle_builtin(argv, envp, env_lst);
+	if (pid == 0)
+	{
+		dup2(*prevpipe, STDIN_FILENO);
+		close(*prevpipe);
+		pathname = get_pathname(argv, env_lst);
+		if (execve(pathname, argv, envp) == -1)
+			exit(0);
+	}
+	else
+	{
+		close(*prevpipe);
+		while (wait(NULL) != -1)
+			;
+	}
+}
+
 void	handle_execution(t_tokenl *token_lst, t_envl *env_lst)
 {
 	char	**argv;
 	char	**envp;
-	char	*pathname;
-	pid_t	pid;
 	int		prevpipe;
 
 	prevpipe = dup(STDIN_FILENO);
@@ -67,29 +89,10 @@ void	handle_execution(t_tokenl *token_lst, t_envl *env_lst)
 			free(dequeue_token(token_lst));
 		envp = list_to_envp(env_lst);
 		argv = get_next_argv(token_lst);
-		if (handle_builtin(argv, envp, env_lst) == -1)
-		{
-			if (token_lst->head)
-				handle_pipe(argv, envp, env_lst, &prevpipe);
-			else
-			{
-				pid = fork();
-				if (pid == 0)
-				{
-					dup2(prevpipe, STDIN_FILENO);
-					close(prevpipe);
-					pathname = get_pathname(argv, env_lst);
-					if (execve(pathname, argv, envp) == -1)
-						exit(0);
-				}
-				else
-				{
-					close(prevpipe);
-					while (wait(NULL) != -1)
-						;
-				}
-			}
-		}
+		if (token_lst->head)
+			handle_pipe(argv, envp, env_lst, &prevpipe);
+		else
+			handle_last_cmd(argv, envp, env_lst, &prevpipe);
 		free(argv);
 		deep_free(envp);
 	}
